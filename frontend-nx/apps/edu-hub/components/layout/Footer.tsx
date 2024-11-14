7f844421b76355b6a56df09d648036fa1e0bd65d
Sina-CAU <sina.luecke@wtnet.de>
2024-11-14 14:07:59 +0100

    Added two Logos to the Footer and adjusted positioning

--- frontend-nx/apps/edu-hub/components/layout/Footer.tsx
+++ frontend-nx/apps/edu-hub/components/layout/Footer.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 import { FC } from 'react';
-
 import facebookIcon from '../../public/images/share/facebook-icon.svg';
 import instagramIcon from '../../public/images/share/instagram-icon.svg';
+import dlcLogo from '../../public/images/share/logo_dlc.svg';
+import foerderLogo from '../../public/images/share/foerderlogos_2.svg';
 
 export const Footer: FC = () => {
   return (
@@ -13,24 +14,56 @@ export const Footer: FC = () => {
           <br />
           <span className="text-xl font-light">by opencampus.sh</span>
         </h3>
-        <div className="mt-6 sm:mt-10 mb-16 sm:mb-20 text-sm font-thin">
-          <p>
-            <a href="/impressum" target="_blank" rel="noopener noreferrer">
-              {'Impressum - Datenschutz'}
-            </a>
-          </p>
-          <p>
-            <a href="https://opencampus.gitbook.io/faq/" target="_blank" rel="noopener noreferrer">
-              {'FAQ'}
-            </a>
-          </p>
-          <p>
-            <a href="https://opencampus.substack.com" target="_blank" rel="noopener noreferrer">
-              {'Newsletter'}
-            </a>
-          </p>
+        
+        <div className="mt-6 sm:mt-2 flex flex-col sm:flex-row sm:justify-between relative">
+
+          <div className="text-sm font-thin order-2 sm:order-1 mt-4 sm:mt-20">
+            <p>
+              <a href="/impressum" target="_blank" rel="noopener noreferrer">
+                {'Impressum - Datenschutz'}
+              </a>
+            </p>
+            <p>
+              <a href="https://opencampus.gitbook.io/faq/" target="_blank" rel="noopener noreferrer">
+                {'FAQ'}
+              </a>
+            </p>
+            <p>
+              <a href="https://opencampus.substack.com" target="_blank" rel="noopener noreferrer">
+                {'Newsletter'}
+              </a>
+            </p>
+          </div>
+          
+          <div className="flex flex-col order-1 sm:order-2 mt-8 sm:-mt-14">
+            <span className="text-white text-sm font-medium tracking-wide mb-2">
+              Gefördert durch:
+            </span>
+           
+            <div className="flex flex-col sm:flex-row items-center sm:items-end gap-8 sm:gap-4 bg-white p-8 sm:p-4 rounded">
+              <div className="flex items-end">
+                <Image 
+                  src={dlcLogo} 
+                  alt="DLC Logo" 
+                  width={200} 
+                  height={80}
+                  className="object-contain"
+                />
+              </div>
+              <div className="flex items-end">
+                <Image 
+                  src={foerderLogo} 
+                  alt="Förder Logo" 
+                  width={400} 
+                  height={80}
+                  className="object-contain"
+                />
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex justify-between items-end">
+
+        <div className="flex justify-between items-end mt-8">
           <span className="text-sm font-light">© 2010 — {new Date().getFullYear()}</span>
           <div className="flex items-center">
             <a href="https://www.instagram.com/opencampus_sh/" target="_blank" rel="noopener noreferrer">
