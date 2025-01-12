531d45bd185f527711513456aea13fdbd7a69edc
steffen74 <steffen@opencampus.sh>
2025-01-12 23:18:19 +0100

    fixes permission error and removed unnecessary code

--- frontend-nx/apps/edu-hub/pages/manage/achievement-options/index.tsx
+++ frontend-nx/apps/edu-hub/pages/manage/achievement-options/index.tsx
@@ -10,16 +10,9 @@ import { useAdminQuery } from '../../../hooks/authedQuery';
 import { useIsAdmin, useIsInstructor, useIsLoggedIn } from '../../../hooks/authentication';
 import { useKeycloakUserProfile, useUserId } from '../../../hooks/user';
 import { ACHIEVEMENT_RECORD_TYPES } from '../../../queries/achievementOption';
-import { ADMIN_COURSE_LIST } from '../../../queries/courseList';
 import { AchievementRecordTypes } from '../../../queries/__generated__/AchievementRecordTypes';
-import {
-  AdminCourseList,
-  AdminCourseListVariables,
-  AdminCourseList_Course,
-} from '../../../queries/__generated__/AdminCourseList';
 
 const AchievementOptions: FC = () => {
-  const [course, setCourse] = useState(undefined as AdminCourseList_Course);
   const [recordTypes, setRecordTypes] = useState([] as string[]);
 
   const isAdmin = useIsAdmin();
@@ -29,24 +22,6 @@ const AchievementOptions: FC = () => {
   const router = useRouter();
   const userId = useUserId();
   const profile = useKeycloakUserProfile();
-  const courseID: number = parseInt(router.query.courseId as string, 10); // {"courseId": 0}
-
-  const query = useAdminQuery<AdminCourseList, AdminCourseListVariables>(ADMIN_COURSE_LIST, {
-    variables: {
-      limit: QUERY_LIMIT,
-      where: {
-        id: { _eq: courseID },
-      },
-    },
-    skip: courseID <= 0,
-  });
-
-  useEffect(() => {
-    const c = [...(query.data?.Course || [])];
-    if (c.length > 0) {
-      setCourse(c[0]);
-    }
-  }, [query.data?.Course]);
 
   const achievementRecordTypesAPI = useAdminQuery<AchievementRecordTypes>(ACHIEVEMENT_RECORD_TYPES);
 
@@ -71,7 +46,6 @@ const AchievementOptions: FC = () => {
                 achievementRecordTypes={recordTypes}
                 userId={userId}
                 userProfile={profile}
-                course={course}
               />
             )}
           </div>
